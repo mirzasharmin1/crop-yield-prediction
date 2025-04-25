9c746305d56fd13dd2c143514a9c68544096e332
Mirza Sharmin <mirza.sharmin1@gmail.com>
2025-04-25 23:33:33 +0100

    Added all Data Reader Function

--- src/utils.py
+++ src/utils.py
@@ -9,9 +9,33 @@ def get_country_from_coordinates(lat, lon, country_df):
 
         dlat = lat - row['latitude']
         dlon = lon - row['longitude']
-        distance_deg = np.sqrt(dlat**2 + dlon**2)
+        distance_deg = np.sqrt(dlat ** 2 + dlon ** 2)
 
         if distance_deg <= row['radius']:
             return row['country']
 
     return "Unknown Location"
+
+
+def add_country_to_df(df, countries_df, lat_col_name='latitude', lon_col_name='longitude'):
+    latitudes = df[lat_col_name].to_numpy()
+    longitudes = df[lon_col_name].to_numpy()
+
+    country_lats = countries_df['latitude'].to_numpy()
+    country_lons = countries_df['longitude'].to_numpy()
+    country_radii = countries_df['radius'].to_numpy()
+    country_names = countries_df['country'].to_numpy()
+
+    result = np.full(latitudes.shape, 'Unknown Location', dtype=object)
+
+    for i in range(len(countries_df)):
+        dlat = latitudes - country_lats[i]
+        dlon = longitudes - country_lons[i]
+        distances = np.sqrt(dlat ** 2 + dlon ** 2)
+
+        within_radius = (distances <= country_radii[i]) & (result == 'Unknown Location')
+
+        result[within_radius] = country_names[i]
+
+    df['country'] = result
+    return df
